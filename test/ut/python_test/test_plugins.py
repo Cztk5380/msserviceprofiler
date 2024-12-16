759200c492e4339d9f30078697a4b3be7e876955
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2024-12-16 16:53:26 +0800

    Match-id-a78ec2b4b074440767fdac262de677ebcc63b157

--- test/ut/python_test/test_plugins.py
+++ test/ut/python_test/test_plugins.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-sys.path.append('..')
-
 import unittest
+
 from ms_service_profiler.plugins.base import PluginBase
 from ms_service_profiler.plugins.sort_plugins import sort_plugins, DependencyNotFoundError, DependencyCycleError
 
