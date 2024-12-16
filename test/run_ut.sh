fe95517cf5a89732445be88ec2fb3f99fb62307a
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2024-12-16 14:16:20 +0800

    Match-id-f6ad3a5dd43d1d08212b260bc96c4aa7ac113cdd

--- test/run_ut.sh
+++ test/run_ut.sh
@@ -2,21 +2,15 @@
 # This script is used to run ut and st testcase.
 # Copyright Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
 CUR_DIR=$(dirname $(readlink -f $0))
-TOP_DIR=${CUR_DIR}/..
+TOP_DIR=$(readlink -f ${CUR_DIR}/..)
 TEST_DIR=${TOP_DIR}/"test"
 SRC_DIR=${TOP_DIR}/"src"
-COMPILE_FLAG=0
 
 clean() {
   cd ${TEST_DIR}
-  if [ -e ${TEST_DIR}/st_report.xml ]; then
-    rm st_report.xml
-    echo "remove last st_report success"
-  fi
-
-  if [ -e ${TEST_DIR}/report ]; then
-    rm -r ${TEST_DIR}/report
-    echo "remove last ut_report success"
+  if [ -e ${TEST_DIR}/coverage.xml ]; then
+    rm coverage.xml
+    echo "remove last coverage.xml success"
   fi
   cd -
 }
@@ -31,7 +25,7 @@ run_test_python() {
   export PYTHONPATH=${TOP_DIR}:${PYTHONPATH}
   python3 -m coverage run --branch --source ${TOP_DIR}/'ms_server_profiler',${TOP_DIR}/'ms_server_profiler_analyze' -m pytest ${TEST_DIR}/ut/python_test
   python3 -m coverage report
-  python3 -m coverage xml -o coverage.xml
+  python3 -m coverage xml -o ${TEST_DIR}/coverage.xml
 }
 
 run_test() {
