58aa386f6b471979f8019ff4d7520ead17b70217
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2024-12-16 21:14:32 +0800

    Match-id-38e49898c156d5f604e75b4c06466530e568f5b5

--- ms_service_profiler/__init__.py
+++ ms_service_profiler/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ms_service_profiler.signals import Span, Metric, Event, ResLink
+from ms_service_profiler.signals import Profiler
