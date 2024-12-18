7c789252c3da65b573434f469e90e336267f479e
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2024-12-18 16:00:47 +0800

    Match-id-2067a08659f9be81482200cfd1541b5285c4e5fe

--- ms_service_profiler/plugins/plugin_req_status.py
+++ ms_service_profiler/plugins/plugin_req_status.py
@@ -58,13 +58,17 @@ def increase_value_to_real_value(data):
         if name == "httpReq":
             cur[0] += 1
         elif name == "ReqState":
-            for j, _ in enumerate(data['req_status_inc_df'].columns[2:]):
-                if cur[j] is None:
-                    continue
-                if data['req_status_df'].iloc[i, 1+j] is not None:
-                    cur[j] += data['req_status_inc_df'].iloc[i, 1+j]
-                    data['req_status_df'].iloc[i, 1+j] = cur[j]
+            sdf(data, cur)
+        data['req_status_df'].iloc[i, 1+j] = cur[j]
 
+
+def sdf(data, cur):
+    for j, _ in enumerate(data['req_status_inc_df'].columns[2:]):
+        if cur[j] is None:
+            continue
+        if data['req_status_df'].iloc[i, 1+j] is not None:
+            cur[j] += data['req_status_inc_df'].iloc[i, 1+j]
+            
 
 def is_metric(name):
     if name[-1] in ['+', '=']:
