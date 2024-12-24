8ed31e0874f0c73c98cea0bd3d4b21949c91b36a
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2024-12-24 10:02:05 +0800

    Match-id-bd7e2e2aeb5332922496df4b0657f37f66fea538

--- ms_service_profiler/exporters/exporter_batch.py
+++ ms_service_profiler/exporters/exporter_batch.py
@@ -17,7 +17,6 @@ from enum import Enum
 from pathlib import Path
 import json
 import pandas as pd
-from matplotlib import pyplot as plt
 
 from ms_service_profiler.exporters.base import ExporterBase
 from ms_service_profiler.parse import save_dataframe_to_csv
@@ -36,23 +35,15 @@ class ExporterBatchData(ExporterBase):
         if df is None:
             logging.error("The data is empty, please check")
             return
-        batch_df = df[df['name'] == 'BatchSchedule']
-        modelexec_df = df[df['name'] == 'modelExec']
-        batch_df_copy = batch_df.copy()
-        modelexec_df_copy = modelexec_df.copy()
-        # 在副本上进行操作
-        batch_df_copy.loc[:, 'resList'] = batch_df_copy['message'].apply(lambda x: x['rid'])
-        modelexec_df_copy.loc[:, 'resList'] = modelexec_df_copy['message'].apply(lambda x: x['rid'])
-        result_df = pd.concat([batch_df_copy, modelexec_df_copy], ignore_index=True)
-        result_df = result_df.sort_values(by='start_time')
-        model_df = result_df[['name', 'resList', 'start_time', 'end_time', 'batch_size', 'batch_type', 'during_time',]]
+        batch_df = df[(df['name'] == 'BatchSchedule') | (df['name'] == 'modelExec')]
+
+        model_df = batch_df[['name', 'res_list', 'start_time', 'end_time', 'batch_size', 'batch_type', 'during_time',]]
         model_df = model_df.rename(columns={
-            'resList': 'res_list',
             'start_time': 'start_time(microsecond)',
             'end_time': 'end_time(microsecond)',
             'during_time': 'during_time(microsecond)'
         })
 
         output = cls.args.output_path
-        save_dataframe_to_csv(model_df, output, "batch.csv")
 
+        save_dataframe_to_csv(model_df, output, "batch.csv")
