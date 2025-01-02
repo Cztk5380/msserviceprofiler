6ac4e7ab061588b713177b71325cfac03dfa2801
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-01-02 10:45:02 +0800

    Match-id-ad2ca2e9eeff547f0161595695823fb6bf84df8f

--- test/st/analyze/test_analyze_cmd_ms_service_profiler.py
+++ test/st/analyze/test_analyze_cmd_ms_service_profiler.py
@@ -8,12 +8,10 @@ from unittest import TestCase
 from test.st.utils import execute_cmd
 
 
-
 class TestAnalyzeCmd(TestCase):
-    ST_DATA_PATH = os.getenv("MS_SERVICE_PROFILER",
-                             "/data/ms_service_profiler")
-    INPUT_PATH = os.path.join(ST_DATA_PATH, "1230-1148-100Req")
-    OUTPUT_PATH = os.path.join(ST_DATA_PATH, "test")
+    ST_DATA_PATH = os.getenv("MS_SERVICE_PROFILER", "/data/ms_service_profiler")
+    INPUT_PATH = os.path.join(ST_DATA_PATH, "analyze/1230-1148-100Req")
+    OUTPUT_PATH = os.path.join(ST_DATA_PATH, "analyze")
     COMMAND_SUCCESS = 0
     ANALYZE_PROFILER = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")),
                                          "ms_service_profiler/analyze.py")
