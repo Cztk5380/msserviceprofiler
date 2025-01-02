d88e1942b3ff08829df9ec9ff1dc9c13027562bc
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-01-02 20:26:37 +0800

    Match-id-1f94354a604ff06372c184e8ff06af07f8e5e4f2

---
+++ src/NpuMemoryUsage.cpp
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <vector>
+#include <dlfcn.h>
+#include "msServiceProfiler/NpuMemoryUsage.h"
+
+namespace msServiceProfiler {
+
+NpuMemoryUsage::NpuMemoryUsage()
+{
+    handleDcmi = dlopen("libdcmi.so", RTLD_LAZY | RTLD_LOCAL);
+    if (handleDcmi == nullptr) {
+        return;
+    }
+}
+NpuMemoryUsage::~NpuMemoryUsage()
+{
+    if (handleDcmi != nullptr) {
+        dlclose(handleDcmi);
+        handleDcmi = nullptr;
+    }
+}
+int NpuMemoryUsage::DcmiInit()
+{
+    using DcmiInitFunc = int (*)();
+    if (handleDcmi == nullptr) {
+        return 1;
+    }
+
+    DcmiInitFunc dcmiInit = (DcmiInitFunc)dlsym(handleDcmi, "dcmi_init");
+    int ret = dcmiInit();
+    return ret;
+}
+
+int NpuMemoryUsage::DcmiGetCardList(int *cardNum, int *cardList, int listLen)
+{
+    using DcmiGetCardListFunc = int (*)(int *, int *, int);
+    if (handleDcmi == nullptr) {
+        return 1;
+    }
+    DcmiGetCardListFunc dcmiGetCardList = (DcmiGetCardListFunc)dlsym(handleDcmi, "dcmi_get_card_list");
+    int ret = dcmiGetCardList(cardNum, cardList, listLen);
+    return ret;
+}
+
+int NpuMemoryUsage::DcmiGetDeviceIdInCard(int cardId, int *deviceIdMax)
+{
+    using DcmiGetDeviceIdInCardFunc = int (*)(int, int *, int *, int *);
+    if (handleDcmi == nullptr) {
+        return 1;
+    }
+    DcmiGetDeviceIdInCardFunc dcmiGetDeviceIdInCard =
+        (DcmiGetDeviceIdInCardFunc)dlsym(handleDcmi, "dcmi_get_device_id_in_card");
+    int mcuId = 0;
+    int cpuId = 0;
+    int ret = dcmiGetDeviceIdInCard(cardId, deviceIdMax, &mcuId, &cpuId);
+    return ret;
+}
+
+int NpuMemoryUsage::DcmiGetDeviceMemoryInfoV3(int cardId, int deviceId, struct dcmi_get_memory_info_stru *memoryInfo)
+{
+    using DcmiGetDeviceMemoryInfoV3Func = int (*)(int, int, dcmi_get_memory_info_stru *);
+    if (handleDcmi == nullptr) {
+        return 1;
+    }
+    DcmiGetDeviceMemoryInfoV3Func dcmiGetDeviceMemoryInfoV3 =
+        (DcmiGetDeviceMemoryInfoV3Func)dlsym(handleDcmi, "dcmi_get_device_memory_info_v3");
+    int ret = dcmiGetDeviceMemoryInfoV3(cardId, deviceId, memoryInfo);
+    return ret;
+}
+
+int NpuMemoryUsage::DcmiGetDeviceHbmInfo(int cardId, int deviceId, struct dsmi_hbm_info_stru *hbm_info)
+{
+    using DcmiGetDeviceHbmInfoFunc = int(*)(int, int, dsmi_hbm_info_stru *);
+    if (handleDcmi == nullptr) {
+        return 1;
+    }
+    DcmiGetDeviceHbmInfoFunc dcmiGetDeviceHbmInfo =
+        (DcmiGetDeviceHbmInfoFunc) dlsym(handleDcmi, "dcmi_get_device_hbm_info");
+    int ret = dcmiGetDeviceHbmInfo(cardId, deviceId, hbm_info);
+    return ret;
+}
+
+int NpuMemoryUsage::InitDcmiCardAndDevices()
+{
+    int ret = DcmiInit();
+    if (ret != 0) {
+        return ret;
+    }
+
+    int cardNum = 0;
+    int cardList[64] = {0};
+    int listLen = 64;
+
+    ret = DcmiGetCardList(&cardNum, cardList, listLen);
+    if (ret != 0) {
+        return ret;
+    }
+
+    for (int cardId = 0; cardId < cardNum; cardId++) {
+        int deviceIdMax = 0;
+        int curRet = DcmiGetDeviceIdInCard(cardList[cardId], &deviceIdMax);
+        if (curRet != 0) {
+            ret = ret + curRet;
+            continue;
+        }
+        for (int deviceId = 0; deviceId < deviceIdMax; deviceId++) {
+            cardDevices.push_back({cardList[cardId], deviceId});
+        }
+    }
+
+    return ret;
+}
+
+int NpuMemoryUsage::GetByDcmi(std::vector<int> &memoryUsed, std::vector<int> &memoryUtiliza)
+{
+    int ret = 0;
+    for (const auto &cardDevice : cardDevices) {
+        int curRet = 0;  // Could either be getting by DcmiGetDeviceMemoryInfoV3 or DcmiGetDeviceHbmInfo
+        if (not is_hbm_device) { // Global value
+            struct dcmi_get_memory_info_stru memoryInfo;
+            curRet = DcmiGetDeviceMemoryInfoV3(cardDevice.cardId, cardDevice.deviceId, &memoryInfo);
+            if (curRet != 0) {
+                is_hbm_device = true;  // Will try DcmiGetDeviceHbmInfo later, and skip this next time
+            } else {
+                memoryUsed.push_back(memoryInfo.memory_size - memoryInfo.memory_available);
+                memoryUtiliza.push_back(memoryInfo.utiliza);
+            }
+        }
+
+        if (is_hbm_device) {
+            struct dsmi_hbm_info_stru hbm_info;
+            curRet = DcmiGetDeviceHbmInfo(cardDevice.cardId, cardDevice.deviceId, &hbm_info);
+            if (curRet != 0) {
+                ret = ret + curRet;
+                continue;
+            } else {
+                memoryUsed.push_back(hbm_info.memory_usage);
+                memoryUtiliza.push_back(hbm_info.memory_usage * 100 / hbm_info.memory_size);
+            }
+        }
+    }
+    return ret;
+}
+}  // namespace msServiceProfiler
