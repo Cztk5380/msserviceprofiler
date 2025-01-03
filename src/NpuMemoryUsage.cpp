e37e3712147ad9dec81b9594f7006b69d1d5a712
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-01-03 15:13:22 +0800

    Match-id-c1be6cfad4c3b61b48ad713db00ddc782aa61405

--- src/NpuMemoryUsage.cpp
+++ src/NpuMemoryUsage.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2025. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,9 +23,9 @@ namespace msServiceProfiler {
 
 NpuMemoryUsage::NpuMemoryUsage()
 {
-    handleDcmi = dlopen("libdcmi.so", RTLD_LAZY | RTLD_LOCAL);
+    handleDcmi = dlopen("libdcmiaa.so", RTLD_LAZY | RTLD_LOCAL);
     if (handleDcmi == nullptr) {
-        return;
+        std::cerr << "[ERROR] failed to dlopen libdcmi.so" << std::endl;
     }
 }
 NpuMemoryUsage::~NpuMemoryUsage()
