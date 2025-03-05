7d96c361907df3d58da8091453d5480aefec35c0
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-03-05 17:45:10 +0800

    Match-id-3458f413108c1d5e9906a2c47ef88b38dfe5ca0e

--- ms_service_profiler/plugins/plugin_metric.py
+++ ms_service_profiler/plugins/plugin_metric.py
@@ -18,13 +18,18 @@ class PluginMetric(PluginBase):
         tx_data_df = data.get('tx_data_df')
         if tx_data_df is None:
             raise DataFrameMissingError(key="tx_data_df")
+
         metric_cols = [col for col in tx_data_df.columns if is_metric(col)]
+
         missing_col = [col for col in ['name', 'start_time', 'start_datetime'] if col not in tx_data_df.columns]
         if missing_col:
             raise ColumnMissingError(key=missing_col)
 
         metric_data_df = tx_data_df[['start_time', 'start_datetime'] + metric_cols].copy()
-        metric_data_df.loc[tx_data_df['name'] == 'httpReq', 'WAITING+'] = 1.0
+
+        if 'FINISHED+' not in metric_data_df.columns:
+            metric_data_df.loc[tx_data_df['name'] == 'httpReq', 'WAITING+'] = 1.0
+
         if (tx_data_df['name'] == 'decodeReq').any():
             metric_data_df.loc[tx_data_df['name'] == 'decodeReq', 'WAITING+'] = 1.0
 
@@ -32,6 +37,7 @@ class PluginMetric(PluginBase):
         metric_data_df[increase_metric_cols] = cal_increase_value(metric_data_df[increase_metric_cols])
 
         metric_data_df = metric_data_df.rename(columns={col: col[:-1] for col in metric_cols})
+
         data['metric_data_df'] = metric_data_df
         return data
 
@@ -47,3 +53,4 @@ def cal_increase_value(df):
     df = df.fillna(0)
     df = df.cumsum(axis=0)
     return df
+
