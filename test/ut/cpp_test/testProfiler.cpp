8cb30c6c2f0eb043e97bb006d9a386eba501d0ff
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-03-10 22:22:28 +0800

    Match-id-d15356d5c8fb83c3722e653ab6e998355ffcb897

--- test/ut/cpp_test/testProfiler.cpp
+++ test/ut/cpp_test/testProfiler.cpp
@@ -1,10 +1,13 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2025. All rights reserved.
+ */
 
 #include <gtest/gtest.h>
 #include <mockcpp/mockcpp.hpp>
 
 
-int multi(int a, int b) {
-    return a * b;
+int Multi(int paramA, int paramB) {
+    return paramA * paramB;
 }
 
 TEST(TestMock, TestMock) {
