22e05020a8426a8203c15b7ffbaab4e8d42823cb
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-03-27 15:17:36 +0800

    Match-id-a2f8d51905d696e810cdd1bcda6aaabab902467f

--- include/msServiceProfiler/ServiceProfilerManager.h
+++ include/msServiceProfiler/ServiceProfilerManager.h
@@ -24,6 +24,7 @@
 using Json = nlohmann::json;
 
 namespace msServiceProfiler {
+    typedef struct aclprofConfig aclprofConfig;
     class ServiceProfilerManager {
     public:
         static ServiceProfilerManager &GetInstance();
