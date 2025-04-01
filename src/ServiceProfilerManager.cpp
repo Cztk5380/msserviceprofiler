9de370f05ae65d13b87dc1c77e6654499265c41a
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-04-01 19:19:31 +0800

    Match-id-07b6008bebcaf0e1c480a8d584cd528c44383e18

--- src/ServiceProfilerManager.cpp
+++ src/ServiceProfilerManager.cpp
@@ -28,7 +28,6 @@
 #include "acl/acl.h"
 #include "mstx/ms_tools_ext.h"
 #include "securec.h"
-#include "file.h"
 
 #include "../include/msServiceProfiler/NpuMemoryUsage.h"
 #include "../include/msServiceProfiler/Profiler.h"
@@ -132,11 +131,7 @@ namespace msServiceProfiler {
     static inline unsigned long Str2Uint(const std::string &str)
     {
         char *endPtr;
-        long ret = std::strtoul(str.c_str(), &endPtr, STRING_TO_UINT_BASE);
-        if (ret == ULONG_MAX) {
-            ret = 0;
-        }
-        return ret;
+        return std::strtoul(str.c_str(), &endPtr, STRING_TO_UINT_BASE);
     }
 
     static inline std::pair<std::string, std::string> SplitStr(const std::string &str, char splitChar)
@@ -205,22 +200,10 @@ namespace msServiceProfiler {
 
     void ServiceProfilerManager::ReadConfigPath()
     {
-        const int maxFileSize = 1048576; // 1MB
         configPath_ = getenv("SERVICE_PROF_CONFIG_PATH") ? getenv("SERVICE_PROF_CONFIG_PATH") : "";
         if (configPath_.empty()) {
             configPath_ = getenv("PROF_CONFIG_PATH") ? getenv("PROF_CONFIG_PATH") : "";
             if (access(configPath_.c_str(), F_OK) != 0) {
-                configPath_ = "";
-            }
-        }
-        if (not configPath_.empty()) {
-            if (not PathLenCheckValid) {
-                PROF_LOGE("SERVICE_PROF_CONFIG_PATH length invalid..");
-                configPath_ = "";
-                return ;
-            }
-            if (GetFileSize(configPath_) > maxFileSize) {
-                PROF_LOGE("SERVICE_PROF_CONFIG_PATH : %s is too large.", configPath_.c_str());
                 configPath_ = "";
             }
         }
