5407d37c26b8c350987543624803b37e8a14613a
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-05-15 15:17:14 +0800

    Match-id-d6c5f593634fa7c676c51aad5661112e80705bec

--- 3rdparty/Buildmspti.cpp
+++ 3rdparty/Buildmspti.cpp
@@ -1,32 +1,38 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2MSPTI_SUCCESS24-2MSPTI_SUCCESS24. All rights reserved.
  * Description: wrapper header
  * Author: Huawei Technologies Co., Ltd.
- * Create: 2024-12-16
+ * Create: 2MSPTI_SUCCESS24-12-16
  */
 #include "mspti/mspti.h"
+#include <iostream>
 
-msptiResult msptiActivityRegisterCallbacks(void (*func1(uint8_t, size_t, size_t)), void (*func2(uint8_t, size_t, size_t)))
+msptiResult msptiActivityRegisterCallbacks(BufferRequestFunctionPtr, BufferCompleteFunctionPtr)
 {
-    return 0;
+    return MSPTI_SUCCESS;
 }
 
 msptiResult msptiActivityEnable(int kind)
 {
-    return 0;
+    return MSPTI_SUCCESS;
 }
 
-msptiResult msptiActivityGetNextRecord(unit8_t *buffer, size_t validBufferSizeBytes, msptiActivity **record)
+msptiResult msptiActivityGetNextRecord(uint8_t *buffer, size_t validBufferSizeBytes, msptiActivity **record)
 {
-    return 0;
+    return MSPTI_SUCCESS;
 }
 
 msptiResult msptiActivityFlushAll(int kind)
 {
-    return 0;
+    return MSPTI_SUCCESS;
 }
 
-msptiResult msptiSubscribe(int subscriber, int callback, int userdata)
+msptiResult msptiSubscribe(msptiSubscriberHandle *subscriber, int *callback, int *userdata)
 {
-    return 0;
+    return MSPTI_SUCCESS;
+}
+
+msptiResult msptiUnsubscribe(msptiSubscriberHandle subscriber)
+{
+    return MSPTI_SUCCESS;
 }
--- 3rdparty/mspti/mspti.h
+++ 3rdparty/mspti/mspti.h
@@ -9,45 +9,63 @@
 #define MS_SERVICE_PROFILER_MSPTI_H
 
 #include <cstdint>
+#include <iostream>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define MSTX_INVALID_ID 0
-
-typedef uint64_t  mstxRangeId;
-
-#ifndef MSPTI_SUCCESS
-static const int MSPTI_SUCCESS = 0,    // MSPTI执行成功，无错误
-static const int MSPTI_ERROR_INVALID_PARAMETER = 1,    // funcBufferRequested或funcBufferCompleted为NULL时返回，表示MSPTI执行失败
-static const int MSPTI_ERROR_MULTIPLE_SUBSCRIBERS_NOT_SUPPORTED = 2,    // 已存在MSPTI用户时返回，表示MSPTI执行失败
-static const int MSPTI_ERROR_MAX_LIMIT_REACHED = 3,    // Activity Buffer没有更多的Record数据时返回，表示MSPTI执行失败
-static const int MSPTI_ERROR_DEVICE_OFFLINE = 4,    // 无法获取DEVICE侧信息
-static const int MSPTI_ERROR_INNER = 999,    // 无法初始化MSPTI时返回，表示MSPTI执行失败
-static const int MSPTI_ERROR_FOECE_INT = 0x7fffffff;
+#define ACTIVITY_STRUCT_ALIGNMENT 8
+#if defined(_WIN32)
+#define START_PACKED_ALIGNMENT __pragma(pack(push, 1))
+#define PACKED_ALIGNMENT __declspec(align(ACTIVITY_STRUCT_ALIGNMENT))
+#define END_PACKED_ALIGNMENT __pragma(pack(pop))
+#elif defined(__GNUC__)
+#define START_PACKED_ALIGNMENT
+#define PACKED_ALIGNMENT __attribute__((__packed__)) __attribute__((aligned(ACTIVITY_STRUCT_ALIGNMENT)))
+#define END_PACKED_ALIGNMENT
+#else
+#define START_PACKED_ALIGNMENT
+#define PACKED_ALIGNMENT
+#define END_PACKED_ALIGNMENT
 #endif
 
-void mstxMarkA(const char* message, aclrtStream stream);
-mstxRangeId mstxRangeStartA(const char* message, aclrtStream stream);
-void mstxRangeEnd(mstxRangeId id);
-
-typedef int msptiResult;
-typedef int msptiSubscriberHandle;
+struct msptiSubscriber_st;
+typedef struct msptiSubscriber_st *msptiSubscriberHandle;
 
 typedef enum {
-	MSPTI_ACTIVITY_KIND_INVALID = 0,   // 非法值
-	MSPTI_ACTIVITY_KIND_MARKER = 1,   // MSPTI打点能力（标记瞬时时刻）的Activity Record类型，支持最大打点个数为uin32_t最大值，调用结构体msptiActivityMarker
-	MSPTI_ACTIVITY_KIND_KERNEL = 2,   // aclnn场景下，计算类算子信息采集的Activity Record类型，调用结构体msptiActivityKernel
-	MSPTI_ACTIVITY_KIND_API = 3,   // aclnn场景下，aclnn组件信息采集Activity Record类型，调用结构体msptiActivityApi
-	MSPTI_ACTIVITY_KIND_HCCL = 4   // 通信算子采集Activity Record类型，调用结构体msptiActivitHccl
+    /**
+    * The activity record is invalid.
+    */
+    MSPTI_ACTIVITY_KIND_INVALID = 0,
+    MSPTI_ACTIVITY_KIND_MARKER = 1,
+    MSPTI_ACTIVITY_KIND_KERNEL = 2,
+    MSPTI_ACTIVITY_KIND_API = 3,
+    MSPTI_ACTIVITY_KIND_HCCL = 4,
+    MSPTI_ACTIVITY_KIND_MEMORY = 5,
+    MSPTI_ACTIVITY_KIND_MEMSET = 6,
+    MSPTI_ACTIVITY_KIND_MEMCPY = 7,
+    MSPTI_ACTIVITY_KIND_EXTERNAL_CORRELATION = 8,
+    MSPTI_ACTIVITY_KIND_COUNT,
+    MSPTI_ACTIVITY_KIND_FORCE_INT = 0x7fffffff
 } msptiActivityKind;
 
-typedef struct msptiActivity {
-	msptiActivityKind kind;   // Activity类型
-};
+typedef enum {
+    MSPTI_SUCCESS                                       = 0,
+    MSPTI_ERROR_INVALID_PARAMETER                       = 1,
+    MSPTI_ERROR_MULTIPLE_SUBSCRIBERS_NOT_SUPPORTED      = 2,
+    MSPTI_ERROR_MAX_LIMIT_REACHED                       = 3,
+    MSPTI_ERROR_DEVICE_OFFLINE                          = 4,
+    MSPTI_ERROR_QUEUE_EMPTY                             = 5,
+    MSPTI_ERROR_INNER                                   = 999,
+    MSPTI_ERROR_FOECE_INT                               = 0x7fffffff
+} msptiResult;
 
-typedef struct msptiActivityApi {
+typedef struct PACKED_ALIGNMENT {
+    msptiActivityKind kind;
+} msptiActivity;
+
+typedef struct PACKED_ALIGNMENT {
 	msptiActivityKind kind;   // Activity Record类型MSPTI_ACTIVITY_KIND_API
 	uint64_t start;   // API执行的开始时间戳，单位ns。开始和结束时间戳均为0时则无法收集API的时间戳信息
 	uint64_t end;   // API执行的结束时间戳，单位ns。开始和结束时间戳均为0时则无法收集API的时间戳信息
@@ -57,9 +75,9 @@ typedef struct msptiActivityApi {
 	} pt;
 	uint64_t correlationId;   // API的关联ID。每个API执行都被分配一个唯一的关联ID，该关联ID与启动API的驱动程序或运行时API Activity Record的关联ID相同
 	const char* name;   // API的名称，该名称在整个Activity Record中保持一致，不建议修改
-};
+} msptiActivityApi;
 
-typedef struct msptiActivitHccl {
+typedef struct PACKED_ALIGNMENT {
 	msptiActivityKind kind;   // Activity Record类型MSPTI_ACTIVITY_KIND_HCCL
 	uint64_t start;   // 通信算子在NPU设备上执行开始时间戳，单位ns。开始和结束时间戳均为0时则无法收集通信算子的时间戳信息
 	uint64_t end;   // 通信算子执行的结束时间戳，单位ns。开始和结束时间戳均为0时则无法收集通信算子的时间戳信息
@@ -70,9 +88,9 @@ typedef struct msptiActivitHccl {
 	uint64_t bandWidth;   // 通信算子运行时的带宽，单位GB/s
 	const char *name;   // 通信算子的名称
 	const char *commName;   // 通信域的名称
-};
+} msptiActivityHccl;
 
-typedef struct msptiActivityKernel {
+typedef struct PACKED_ALIGNMENT {
 	msptiActivityKind kind;   // Activity Record类型MSPTI_ACTIVITY_KIND_KERNEL
 	uint64_t start;   // Kernel在NPU设备上执行开始时间戳，单位ns。开始和结束时间戳均为0时则无法收集kernel的时间戳信息
 	uint64_t end;   // kernel执行的结束时间戳，单位ns。开始和结束时间戳均为0时则无法收集kernel的时间戳信息
@@ -83,13 +101,17 @@ typedef struct msptiActivityKernel {
 	uint64_t correlationId;   // Runtime在Launch Kernel时生成的唯一ID，其它Activity可通过该值与Kernel进行关联
 	const char *type;   // kernel的类型
 	const char *name;   // kernel的名称，该名称在整个Activity Record中保持一致，不建议修改
-};
+} msptiActivityKernel;
 
-msptiResult msptiActivityRegisterCallbacks(void (*func1(uint8_t, size_t, size_t)), void (*func2(uint8_t, size_t, size_t)));
+typedef void (*BufferRequestFunctionPtr)(uint8_t**, size_t*, size_t*);
+typedef void (*BufferCompleteFunctionPtr)(uint8_t*, size_t, size_t);
+
+msptiResult msptiActivityRegisterCallbacks(BufferRequestFunctionPtr, BufferCompleteFunctionPtr);
 msptiResult msptiActivityEnable(int);
-msptiResult msptiActivityGetNextRecord(unit8_t *buffer, size_t validBufferSizeBytes, msptiActivity **record);
+msptiResult msptiActivityGetNextRecord(uint8_t*, size_t, msptiActivity**);
 msptiResult msptiActivityFlushAll(int);
-msptiResult msptiSubscribe(int, int, int);
+msptiResult msptiSubscribe(msptiSubscriberHandle*, int*, int*);
+msptiResult msptiUnsubscribe(msptiSubscriberHandle);
 
 #ifdef __cplusplus
 }
