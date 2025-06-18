68bc912d1bee0171c8a047b7abed0ed9d259119a
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-06-18 18:07:36 +0800

    Match-id-7dba94aa290a8165d3c09a755ff60735b4415b8f

--- src/ServiceProfilerManager.cpp
+++ src/ServiceProfilerManager.cpp
@@ -656,7 +656,7 @@ namespace msServiceProfiler {
         }
 
         config_->SetEnable(false);
-        if (npuFlag_ | msptiEnabled) {
+        if (npuFlag_ || msptiEnabled) {
             StopAclTaskTime();
         }
 
