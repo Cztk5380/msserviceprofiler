85723c0687b7b889592b036f662489e7bcebc8a8
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-07-03 14:51:57 +0800

    Match-id-983173351c9bfdc934a9cbb417825ebc4cd6d910

--- test/run_ut.sh
+++ test/run_ut.sh
@@ -87,7 +87,7 @@ run_test_cpp() {
     exit 1
   fi
   cd ${TEST_DIR}
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${TEST_DIR}/test_build/3rdparty
+  export LD_LIBRARY_PATH=${TEST_DIR}/test_build/3rdparty:$LD_LIBRARY_PATH
   ./test_build/st_server_profiler & ./test_build/st_server_profiler
   ./test_build/ut_server_profiler
 
