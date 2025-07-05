f64c3341632a983f504de1acf2ce3a3e34f1d174
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-07-05 15:10:26 +0800

    Match-id-a575a7226c9657ac2f4964ca57924bae519e487c

---
+++ test/ut/cpp_test/testConfig.cpp
@@ -0,0 +1,37 @@
+/*
+* Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <gtest/gtest.h>
+#include <vector>
+#include <chrono>
+#include <mockcpp/mockcpp.hpp>
+#include <nlohmann/json.hpp>
+#include <fstream>
+
+#include "acl/acl_prof.h"
+#include "acl/acl.h"
+
+#include "msServiceProfiler/Utils.h"
+#include "msServiceProfiler/Config.h"
+#include "stubs.h"
+
+using namespace ::testing;
+
+void MarkEventLongAttr(const char *msg);
+namespace msServiceProfiler { void Write2Tx(const std::vector<int> &memoryInfo, const std::string metricName); }
+
+TEST(ProfilerTest, TestParseMspti)
+{
+    nlohmann::json configTest1 = nlohmann::json::object();
+    nlohmann::json configTest2 = nlohmann::json::object();
+    configTest1["api_filter"] = "";
+    configTest1["kernel_filter"] = "";
+    configTest2["api_filter"] = 1;
+    configTest2["kernel_filter"] = 1;
+
+    EXPECT_NO_THROW(ParseMspti(configTest1));
+    EXPECT_NO_THROW(ParseMspti(configTest2));
+}
