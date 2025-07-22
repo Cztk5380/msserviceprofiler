52ae33e22396006ef21f0983c4fd446328557d58
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-07-22 21:47:28 +0800

    Match-id-516010851a78d84c9cf7fc6cb5a482143b020ad8

--- ms_service_profiler/exporters/exporter_req_status.py
+++ ms_service_profiler/exporters/exporter_req_status.py
@@ -21,76 +21,88 @@ class ExporterReqStatus(ExporterBase):
     @timer(logger.info)
     @key_except('domain', 'name', ignore=True, msg="ignoring current exporter by default.")
     def export(cls, data) -> None:
-        if 'db' in cls.args.format:
-            df = data.get('tx_data_df')
-            if df is None:
-                logger.warning("The data is empty, please check")
-                return
+        if 'db' not in cls.args.format:
+            return
 
-            if check_domain_valid(df, ['Request'], 'request_status') is False:
-                return
+        df = data.get('tx_data_df')
+        if df is None:
+            logger.warning("The data is empty, please check")
+            return
 
-            metrics = data.get('metric_data_df')
-            if metrics is None:
-                logger.warning("The metrics data is empty, please check")
-                return
+        if not check_domain_valid(df, ['Request'], 'request_status'):
+            return
 
-            # mindIE 重构后新逻辑
-            if 'status' in df.columns:
-                old_status_mapping = {
-                    'waiting': 'WAITING',
-                    'running': 'RUNNING',
-                }
+        metrics = data.get('metric_data_df')
+        if metrics is None:
+            logger.warning("The metrics data is empty, please check")
+            return
 
-                # 将status列的值映射到旧版状态值
-                df['status'] = df['status'].map(old_status_mapping)
-
-                # 将status列转换为one-hot编码
-                df = pd.get_dummies(df['status'], prefix='', prefix_sep='')
-
-                # 添加timestamp列
-                df.insert(0, 'timestamp', metrics['start_datetime'])
-
-                # 补全缺失的状态列，值为0
-                for status in old_status_mapping.values():
-                    if status not in df.columns:
-                        df[status] = 0
-
-                if 'PENDING' not in df.columns:
-                    df['PENDING'] = 0
-
-                # 确保列的顺序正确
-                df = df[['timestamp'] + list(old_status_mapping.values()) + ['PENDING']]
-
-            else:
-                req_status_cols = [col for col in metrics.columns if col in ReqStatus.__members__]
-
-                df = metrics[req_status_cols].astype(int)
-                df.insert(0, 'timestamp', metrics['start_datetime'])
-
-                # 默认会从db文件中筛选下述列进行展示，如不存在该列需要补齐
-                show_columns = []
-                for status in ReqStatus:
-                    show_columns.append(status.name)
-
-                for column_name in show_columns:
-                    if column_name not in df.columns:
-                        df = df.assign(**{column_name: [None] * len(df)})
+        # 处理 status 列的映射和编码
+        df = cls._process_status_columns(df, metrics)
 
         write_result_to_db(
             df_param_list=[[df, 'request_status']],
             table_name='request_status',
-            create_view_sql=[CREATE_REQUEST_STATE_VIEW_SQL]
+            create_view_sql=[cls.CREATE_REQUEST_STATE_VIEW_SQL]
         )
 
+    @classmethod
+    def _process_status_columns(cls, df, metrics):
+        if 'status' in df.columns:
+            df = cls._map_and_encode_status(df, metrics)
+        else:
+            df = cls._prepare_metrics_df(df, metrics)
+        return df
 
-CREATE_REQUEST_STATE_VIEW_SQL = f"""
-    CREATE VIEW {CURVE_VIEW_NAME_LIST['request_status']} AS
-    SELECT
-        substr( timestamp, 1, 10 ) || ' ' || substr( timestamp, 12, 8 ) || '.' || substr( timestamp, 21, 6 ) AS time,
-        WAITING, PENDING, RUNNING 
-    FROM
-        request_status 
-    ORDER BY
-        time ASC
-"""
+    @classmethod
+    def _map_and_encode_status(cls, df, metrics):
+        old_status_mapping = {
+            'waiting': 'WAITING',
+            'running': 'RUNNING',
+        }
+
+        # 将status列的值映射到旧版状态值
+        df['status'] = df['status'].map(old_status_mapping)
+
+        # 将status列转换为one-hot编码
+        df = pd.get_dummies(df['status'], prefix='', prefix_sep='')
+
+        # 添加timestamp列
+        df.insert(0, 'timestamp', metrics['start_datetime'])
+
+        # 补全缺失的状态列，值为0
+        for status in old_status_mapping.values():
+            if status not in df.columns:
+                df[status] = 0
+
+        if 'PENDING' not in df.columns:
+            df['PENDING'] = 0
+
+        # 确保列的顺序正确
+        df = df[['timestamp'] + list(old_status_mapping.values()) + ['PENDING']]
+        return df
+
+    @classmethod
+    def _prepare_metrics_df(cls, df, metrics):
+        req_status_cols = [col for col in metrics.columns if col in ReqStatus.__members__]
+        df = metrics[req_status_cols].astype(int)
+        df.insert(0, 'timestamp', metrics['start_datetime'])
+
+        # 默认会从db文件中筛选下述列进行展示，如不存在该列需要补齐
+        show_columns = [status.name for status in ReqStatus]
+
+        for column_name in show_columns:
+            if column_name not in df.columns:
+                df = df.assign(**{column_name: [None] * len(df)})
+        return df
+
+    CREATE_REQUEST_STATE_VIEW_SQL = f"""
+        CREATE VIEW {CURVE_VIEW_NAME_LIST['request_status']} AS
+        SELECT
+            substr( timestamp, 1, 10 ) || ' ' || substr( timestamp, 12, 8 ) || '.' || substr( timestamp, 21, 6 ) AS time,
+            WAITING, PENDING, RUNNING 
+        FROM
+            request_status 
+        ORDER BY
+            time ASC
+    """
