9cc57d88a248f6de6cb6dcb823d88c161b59f901
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-08-21 11:50:06 +0800

    Match-id-9e5d52948262e217e40fbc042a0d72534f83ecc3

--- src/NpuMemoryUsage.cpp
+++ src/NpuMemoryUsage.cpp
@@ -54,14 +54,14 @@ int NpuMemoryUsage::DcmiInit() const
     return ret;
 }
 
-int NpuMemoryUsage::DcmiGetCardList(int *cardNum, int *paramCardList, int listLength) const
+int NpuMemoryUsage::DcmiGetCardList(int *paramCardNum, int *paramCardList, int listLength) const
 {
     using DcmiGetCardListFunc = int (*)(int *, int *, int);
     if (handleDcmi == nullptr) {
         return EXITCODE_EMPTY_DCMI_HANDLER;
     }
     DcmiGetCardListFunc dcmiGetCardList = (DcmiGetCardListFunc)dlsym(handleDcmi, "dcmi_get_card_list");
-    int ret = dcmiGetCardList(cardNum, paramCardList, listLength);
+    int ret = dcmiGetCardList(paramCardNum, paramCardList, listLength);
     return ret;
 }
 
