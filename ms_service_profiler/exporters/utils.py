255c509233ab8b1e84172f5629ceb38d2db2201c
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-09-02 17:12:09 +0800

    Match-id-f1c81582aa9a08c1e648963d1be604c292d0e66e

--- ms_service_profiler/exporters/utils.py
+++ ms_service_profiler/exporters/utils.py
@@ -36,7 +36,7 @@ CURVE_VIEW_NAME_LIST = {
     'decode_gen_speed': 'Decode_Generate_Speed_Latency_curve',
     'first_token_latency': 'First_Token_Latency_curve',
     'request_status': 'Request_Status_curve',
-    'coordinator' : 'Coordinator_curve',
+    'coordinator': 'Coordinator_curve',
 }
 TABLE_DATA_VIEW_NAME_LIST = {
     # 需要以纯表显示的db中的表名: data_table中的视图名称
