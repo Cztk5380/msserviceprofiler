b917aa746ee9f026cb68bb96cce4f62d504d9d45
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-09-06 15:47:41 +0800

    Match-id-f126b61f4e7ecc92c5669f63453890f35222db47

--- test/st/collect/test_pd_competition_acl_prof_task_time_level.py
+++ test/st/collect/test_pd_competition_acl_prof_task_time_level.py
@@ -12,7 +12,7 @@ from test.st.checker.trace_checker import check_chrome_tracing
 from test.st.checker.dump_checker import mindie_key_word_checker
 
 
-def test_example(devices, mindie_path, dataset_path, model_path, tmp_workspace):
+def test_acl_prof_task_time_level_example(devices, mindie_path, dataset_path, model_path, tmp_workspace):
     try:
         workspace_path = tmp_workspace
 
