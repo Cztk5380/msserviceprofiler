c1c46cb82bc09f770d5b8f1d169498bcc73544a8
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-09-06 09:53:44 +0800

    Match-id-01cbe960ecb23952927b654946504915570fb78c

--- test/st/collect/test_pd_competition_timelimit.py
+++ test/st/collect/test_pd_competition_timelimit.py
@@ -1,3 +1,4 @@
+import json
 import os
 import uuid
 
@@ -22,8 +23,14 @@ def test_example(devices, mindie_path, dataset_path, model_path, tmp_workspace):
         mindie_server.set_mindie_path(mindie_path)
         mindie_server.set_model_path(model_path)
         mindie_server.set_prof_config(prof_dir=os.path.join(workspace_path, "prof_data"))
-        mindie_server.set_prof_config(enable=1, time_limit=18)
+        mindie_server.set_prof_config(enable=1, timelimit=18)
         assert mindie_server.ready_go()
+        assert mindie_server.wait("Profiler Timelimit 18 Seconds Is Reached, Profiler Disabled Successfully!", 18)
+        with open(mindie_server.prof_config_path, "r", encoding="utf-8") as f:
+            prof_config = json.load(f)
+        print(type(prof_config))
+        print(prof_config)
+
 
     finally:
         if mindie_server:
