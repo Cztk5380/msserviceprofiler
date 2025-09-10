627ae1e770da8bafdda9393c33d1e67bdb06fe21
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-09-10 15:57:33 +0800

    Match-id-8b97e493a6b85b5cc6d3ce68d0573202cbd247dd

--- test/st/collect/test_auto_make_json.py
+++ test/st/collect/test_auto_make_json.py
@@ -4,11 +4,28 @@ import os
 
 from test.st.executor.exec_benchmark import ExecBenchmark
 from test.st.executor.exec_mindie_server import ExecMindIEServer
-from test.st.checker.npu_cpu_checker import check_npu_cpu
 from test.st.checker.dump_checker import mindie_key_word_checker
 
 
-def test_npu_cpu_example(devices, mindie_path, dataset_path, model_path, tmp_workspace):
+def simplified_ready_go(self):
+    daemon_file = os.path.join(self.mindie_path, "bin", "mindieservice_daemon")
+
+    self.execute(
+        [daemon_file, "--config-file", self.service_config_path], 
+        dict(SERVICE_PROF_CONFIG_PATH=self.prof_config_path, 
+            MINDIE_LOG_TO_STDOUT='1', 
+            MINDIE_LLM_LOG_TO_STDOUT='1'))
+
+    exit_code, has_output = self.wait("Daemon start success!", timeout=600) # 等个10分钟，10分钟都起不来，怕不是卡死了
+
+    print("wait result: ", exit_code, has_output)
+    if exit_code is None and has_output == 0:
+        return True
+    else:
+        return False
+
+
+def test_auto_make_json_file(devices, mindie_path, dataset_path, model_path, tmp_workspace):
     try:
         workspace_path = tmp_workspace
 
@@ -17,23 +34,20 @@ def test_npu_cpu_example(devices, mindie_path, dataset_path, model_path, tmp_wor
         mindie_server.set_device_id(*devices)
         mindie_server.set_mindie_path(mindie_path)
         mindie_server.set_model_path(model_path)
-        mindie_server.set_prof_config(prof_dir=os.path.join(workspace_path, "prof_data"))
-        mindie_server.set_prof_config(enable=1, host_system_usage_freq=1, npu_memory_usage_freq=1)
+
+        if hasattr(mindie_server, 'prof_config_path') and os.path.exists(mindie_server.prof_config_path):
+            os.remove(mindie_server.prof_config_path)
+        
+        mindie_server.ready_go = simplified_ready_go.__get__(mindie_server, ExecMindIEServer)
         assert mindie_server.ready_go()
 
-        # curl 一条试试深浅
-        benchmark = ExecBenchmark()
-        benchmark.set_model_path(model_path)
-        benchmark.set_dataset_path(dataset_path)
-        assert benchmark.curl_test()
-
-        mindie_server.set_prof_config(acl_task_time=0, enable=0)
         mindie_server.kill()
+        service_config_path = os.path.join(workspace_path, "service_config.json")
+        file_exists = os.path.exists(service_config_path)
 
-        mindie_key_word_checker(os.path.join(workspace_path, "prof_data"))
+        print(f"检查路径: {service_config_path}")
+        print(f"文件是否存在: {file_exists}")
 
-        # 开始校验
-        check_npu_cpu(os.path.join(workspace_path, "prof_data"))
     finally:
         if mindie_server:
             mindie_server.kill()
