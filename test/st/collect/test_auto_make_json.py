e032ebacfe3c44aa5a8243f9743a307af5a778d9
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-09-10 17:28:25 +0800

    Match-id-023a5c646e8124c4396bbc65946e95ee600361b6

--- test/st/collect/test_auto_make_json.py
+++ test/st/collect/test_auto_make_json.py
@@ -11,18 +11,13 @@ def simplified_ready_go(self):
     daemon_file = os.path.join(self.mindie_path, "bin", "mindieservice_daemon")
 
     self.execute(
-        [daemon_file, "--config-file", self.service_config_path], 
-        dict(SERVICE_PROF_CONFIG_PATH=self.prof_config_path, 
-            MINDIE_LOG_TO_STDOUT='1', 
+        [daemon_file],
+        dict(MINDIE_LOG_TO_STDOUT='1',
             MINDIE_LLM_LOG_TO_STDOUT='1'))
 
     exit_code, has_output = self.wait("Daemon start success!", timeout=600) # 等个10分钟，10分钟都起不来，怕不是卡死了
 
     print("wait result: ", exit_code, has_output)
-    if exit_code is None and has_output == 0:
-        return True
-    else:
-        return False
 
 
 def test_auto_make_json_file(devices, mindie_path, dataset_path, model_path, tmp_workspace):
@@ -37,16 +32,16 @@ def test_auto_make_json_file(devices, mindie_path, dataset_path, model_path, tmp
 
         if hasattr(mindie_server, 'prof_config_path') and os.path.exists(mindie_server.prof_config_path):
             os.remove(mindie_server.prof_config_path)
+        os.environ['SERVICE_PROF_CONFIG_PATH'] = os.path.join(tmp_workspace, "enable.json")
         
         mindie_server.ready_go = simplified_ready_go.__get__(mindie_server, ExecMindIEServer)
-        assert mindie_server.ready_go()
+        mindie_server.ready_go()
 
         mindie_server.kill()
-        service_config_path = os.path.join(workspace_path, "service_config.json")
+        service_config_path = os.path.join(tmp_workspace, "enable.json")
         file_exists = os.path.exists(service_config_path)
 
-        print(f"检查路径: {service_config_path}")
-        print(f"文件是否存在: {file_exists}")
+        assert file_exists
 
     finally:
         if mindie_server:
