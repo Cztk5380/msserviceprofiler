b5885a16d8962c784f06552d4378c3481baaeefb
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-10-14 17:31:00 +0800

    Match-id-469453597c292e69f1327a3e2f443e889667b307

--- ms_service_profiler/exporters/exporter_trace.py
+++ ms_service_profiler/exporters/exporter_trace.py
@@ -33,7 +33,8 @@ class ExporterTrace(TaskExporterBase):
         data, mspti = self.get_depends_result("pipeline:service", None), self.get_depends_result("pipeline:mspti", None)
         if data is None and mspti is None:
             self.gather((None, None), dst=0)
-            return
+            return None  # 统一返回None
+
         if self.task_index == 0 and (data is not None or mspti is not None):
             data_list = self.gather((None, None), dst=0)
         else:
@@ -54,6 +55,7 @@ class ExporterTrace(TaskExporterBase):
         valid_mspti = all_mspti[0] if all_mspti else None
 
         self.export(valid_data, valid_mspti)
+        return None  # 统一返回None
 
     @classmethod
     @timer(logger.debug)
