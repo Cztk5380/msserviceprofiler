991e12f1f6b0aec7499962b465a06de655fc2401
msserviceprofilerDeveloper <msserviceprofilerDeveloper>
2025-10-27 19:28:33 +0800

    Match-id-e5ee198ee3b582c20d78432626731e73da869c63

--- test/ut/python_test/test_trace_to_db.py
+++ test/ut/python_test/test_trace_to_db.py
@@ -185,7 +185,6 @@ class TestTraceEvent(unittest.TestCase):
         )
         self.cursor.execute.assert_called_with(*expected_call)
 
-
     def test_trans_trace_meta_event_unknown_name(self):
         self.event['name'] = "unknown_name"
         self.event['pid'] = 1
@@ -196,33 +195,37 @@ class TestTraceEvent(unittest.TestCase):
         # 验证没有调用 cursor.execute
         self.cursor.execute.assert_not_called()
 
-
     def test_write_to_process_thread_table(self):
-        self.event['thread_sort_index'] = 1  # 确保 thread_sort_index 是整数 1
+        self.event['thread_sort_index'] = 1
         event_data = trans_trace_meta_data(self.event)
-        write_to_process_thread_table(event_data, 1, self.cursor)
-        self.cursor.execute.assert_called_with(
-            SIMULATION_UPDATE_PROCESS_NAME_SQL, (1, 1)
-        )
-        
+        with patch('ms_service_profiler.utils.trace_to_db.TrackIdManager.get_track_id', return_value=(2, True)):
+            write_to_process_thread_table(event_data, 1, self.cursor)
+            expected_call = (
+                SIMULATION_UPDATE_THREAD_NAME_SQL,
+                (2, 1, 1, 1, 1)  # (track_id, tid, pid, thread_name, thread_sort_index=1)
+            )
+            self.cursor.execute.assert_called_with(*expected_call)
 
     def test_trans_trace_slice_event(self):
         self.event['ph'] = 'X'
-        self.event['thread_sort_index'] = 1  # 确保 thread_sort_index 是整数 1
-        trans_trace_slice_event(self.event, self.cursor)
-        self.cursor.execute.assert_called_with(
-            SIMULATION_UPDATE_PROCESS_NAME_SQL, (1, 1)
-        )
-
+        self.event['thread_sort_index'] = 1
+        with patch('ms_service_profiler.utils.trace_to_db.TrackIdManager.get_track_id', return_value=(2, True)):
+            trans_trace_slice_event(self.event, self.cursor)
+            expected_call = (
+                SIMULATION_UPDATE_THREAD_NAME_SQL,
+                (2, 1, 1, 1, 1)  # (track_id, tid, pid, thread_name, thread_sort_index=1)
+            )
+            self.cursor.execute.assert_called_with(*expected_call)
 
     def test_trans_trace_flow_event(self):
         self.event['ph'] = 's'
-        trans_trace_flow_event(self.event, 's', self.cursor)
-        expected_call = (
-            SIMULATION_UPDATE_PROCESS_NAME_SQL,
-            (1, 1)
-        )
-        self.cursor.execute.assert_called_with(*expected_call)
+        with patch('ms_service_profiler.utils.trace_to_db.TrackIdManager.get_track_id', return_value=(2, True)):
+            trans_trace_flow_event(self.event, 's', self.cursor)
+            expected_call = (
+                SIMULATION_UPDATE_THREAD_NAME_SQL,
+                (2, 1, 1, 1, None)  # (track_id, tid, pid, thread_name, thread_sort_index)
+            )
+            self.cursor.execute.assert_called_with(*expected_call)
 
 
     def test_trans_trace_counter_event(self):
@@ -242,25 +245,25 @@ class TestTraceEvent(unittest.TestCase):
         )
         self.cursor.execute.assert_called_with(*expected_call)
 
-
     def test_trans_trace_event_slice(self):
         self.event['ph'] = 'X'
-        trans_trace_event(self.event, self.cursor)
-        expected_call = (
-            SIMULATION_UPDATE_PROCESS_NAME_SQL,
-            (1, 1)
-        )
-        self.cursor.execute.assert_called_with(*expected_call)
-
+        with patch('ms_service_profiler.utils.trace_to_db.TrackIdManager.get_track_id', return_value=(2, True)):
+            trans_trace_event(self.event, self.cursor)
+            expected_call = (
+                SIMULATION_UPDATE_THREAD_NAME_SQL,
+                (2, 1, 1, 1, None)  # (track_id, tid, pid, thread_name, thread_sort_index)
+            )
+            self.cursor.execute.assert_called_with(*expected_call)
 
     def test_trans_trace_event_counter(self):
         self.event['ph'] = 'C'
-        trans_trace_event(self.event, self.cursor)
-        expected_call = (
-            SIMULATION_UPDATE_PROCESS_NAME_SQL,
-            (1, 1)
-        )
-        self.cursor.execute.assert_called_with(*expected_call)
+        with patch('ms_service_profiler.utils.trace_to_db.TrackIdManager.get_track_id', return_value=(2, True)):
+            trans_trace_event(self.event, self.cursor)
+            expected_call = (
+                SIMULATION_UPDATE_THREAD_NAME_SQL,
+                (2, 'process_name', 1, 'process_name', None)  # (track_id, tid, pid, thread_name, thread_sort_index)
+            )
+            self.cursor.execute.assert_called_with(*expected_call)
 
 
     def test_trans_trace_event_flow(self):
